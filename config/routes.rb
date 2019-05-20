4fd1ef14dde1732dccd5b8d2416d2dd10d2fc567
Romain DESMIDT <romain.desmidt@gmail.com>
2019-05-20 10:16:50 +0200

    change routes to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'sondages#new'
   get "sondages", to: "sondages#index"
-   get "sondages/new", to: "sondages#new"
-   post "sondages", to: "sondages#create"
+   # get "/", to: "sondages#new"
+   post "/", to: "sondages#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
