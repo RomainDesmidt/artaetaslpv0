bd2572e89671a036d216f0ac7d0d0338bf90313d
Romain DESMIDT <romain.desmidt@gmail.com>
2019-05-28 13:15:25 +0200

    sondages#index route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'sondages#new'
-  get "sondages/all/nico", to: "sondages#index"
+  get "2f736f6e64616765732f30313031383a3032303a303130353a3032303a30313031392f616c6c2f/all", to: "sondages#index"
    # get "/", to: "sondages#new"
    post "/", to: "sondages#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
