f6b554a9f1de5a80df53e30b3bbdfcab54657f96
Romain DESMIDT <romain.desmidt@gmail.com>
2019-05-28 12:56:34 +0200

    route nico

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'sondages#new'
-  get "sondages", to: "sondages#index"
+  get "sondages/all/nico", to: "sondages#index"
    # get "/", to: "sondages#new"
    post "/", to: "sondages#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
